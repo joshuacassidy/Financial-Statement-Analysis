f8a7836ad27f1091440e316cf3a4e5139616e7f4
joshuacassidy <Joshcassidy79@gmail.com>
2017-06-03 22:56:38 +0100

    Added and imported the clear function

--- Financial-Statement-Analysis.py
+++ Financial-Statement-Analysis.py
@@ -1,6 +1,13 @@
+import os
+def Clear():
+    if os.name != 'nt':
+        os.system('clear')
+    else:
+        os.system('cls')
 #Data
 def Analysis(revenue = [14574.49, 7606.46, 8611.41, 9175.41, 8058.65, 8105.44, 11496.28, 9766.09, 10305.32, 14379.96, 10713.97, 15433.50]
 ,expenses = [12051.82, 5695.07, 12319.20, 12089.72, 8658.57, 840.20, 3285.73, 5821.12, 6976.93, 16618.61, 10054.37, 3803.96]):
+    Clear()
     profit = [(revenue[i] - expenses [i]) for i in range(len(revenue))]
 
     tax = [round(i * .3, 2) for i in profit]
