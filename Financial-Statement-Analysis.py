a53825dc77be58336f02d6f69ac0864c8b89d1d4
joshuacassidy <Joshcassidy79@gmail.com>
2017-06-03 22:44:48 +0100

    Optimized loops and cleaned up code and output

--- Financial-Statement-Analysis.py
+++ Financial-Statement-Analysis.py
@@ -1,52 +1,24 @@
-#Sample Data 
+#Data 
 revenue = [14574.49, 7606.46, 8611.41, 9175.41, 8058.65, 8105.44, 11496.28, 9766.09, 10305.32, 14379.96, 10713.97, 15433.50]
 expenses = [12051.82, 5695.07, 12319.20, 12089.72, 8658.57, 840.20, 3285.73, 5821.12, 6976.93, 16618.61, 10054.37, 3803.96]
 
-profit = list([])
-
-for i in range (0, len(revenue)):
-    profit.append(revenue[i] - expenses [i])
+profit = [(revenue[i] - expenses [i]) for i in range(len(revenue))]
 
 tax = [round(i * .3, 2) for i in profit]
-
-profit_after_tax = list([])
-for i in range (0, len(profit)):
-    profit_after_tax.append(profit[i] - tax[i])
-
-profit_margin = list([])
-for i in range (0, len(profit)):
-    profit_margin.append(profit_after_tax[i] / revenue[i])
-
-profit_margin = [round((i*100),2) for i in profit_margin]
-
-#Calculate The Mean Profit After Tax For The 12 Months
-mean_pat = sum(profit_after_tax) / len(profit_after_tax)
-
-#Find The Months With Above-Mean Profit After Tax
-good_months = list([])
-for i in range (0, len(revenue)):
-    good_months.append(profit_after_tax[i] > mean_pat)
-
+profit_after_tax = [(profit[i] - tax[i]) for i in range(len(profit))]
+mean_profit_after_tax = sum(profit_after_tax) / len(profit_after_tax)
+profit_margin = [(int(round(((profit_after_tax[i] / revenue[i])*100),0))) for i in range(len(profit))]
 bad_months = list([])
+good_months = list([])
+months_preformance = [good_months.append(i) if profit_after_tax[i] > mean_profit_after_tax else bad_months.append(i) for i in range(len(profit))]
 
-for i in range (0, len(good_months)):
-    bad_months.append(not (good_months[i]))
-    
-best_month = list([])
-worst_month = list([])
+best_month = ' '.join([str(i+1) if profit_after_tax[i] == max(profit_after_tax) else '' for i in range(len(profit))]).strip()
 
-for i in range(0,len(profit_after_tax)):
-    best_month.append(profit_after_tax[i] == max(profit_after_tax))
-    worst_month.append(profit_after_tax[i] == min(profit_after_tax))
+worst_month = ' '.join([str(i+1) if profit_after_tax[i] == min(profit_after_tax) else '' for i in range(len(profit))]).strip()
 
-revenue_1000 = [round(i/1000, 2) for i in revenue]
-expenses_1000 = [round(i/1000, 2) for i in expenses]
-profit_1000 = [round(i/1000, 2) for i in profit]
-profit_after_tax_1000 = [round(i/1000, 0) for i in profit_after_tax]
+revenue_1000 = [int(round(i, 0))for i in revenue]
+expenses_1000 = [int(round(i, 0)) for i in expenses]
+profit_1000 = [int(round(i, 0)) for i in profit]
+profit_after_tax_1000 = [int(round(i, 0)) for i in profit_after_tax]
 
-revenue_1000 = [int(i) for i in revenue_1000]
-expenses_1000 = [int(i) for i in expenses_1000]
-profit_1000 = [int(i) for i in profit_1000]
-profit_after_tax_1000 = [int(i) for i in profit_after_tax]
-
-print ("Revenue :",revenue_1000,"Expenses : ",expenses_1000,"Profit :",profit_1000,"Profit after tax :",profit_after_tax_1000,"Profit margin :",profit_margin,"Good months :",good_months,"Bad months :",bad_months,"Best month :",best_month,"Worst month :",worst_month) 
+print("\nEach months revenue : {0}. \n\nEach months expenses: {1}. \n\nThe profit for each month {2}. \n\nThe profit after tax for each month {3}. \n\nThe profit margin for each month {4}. \n\nThe good months where the profit was above average: {5}.\n\nThe bad months where the profit was below average: {6}.\n\nThe most profitable month {7}.\n\nThe least profitable month {8}.\n".format(revenue_1000,expenses_1000,profit_1000,profit_after_tax_1000,profit_margin,good_months,bad_months,best_month,worst_month))
